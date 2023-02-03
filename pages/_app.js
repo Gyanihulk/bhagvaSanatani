c2ab93a7955acad2fe52a53a29cef88f17c43513
adamya-laptop <kumar.adamya2018@gmail.com>
2023-02-03 18:21:08 +0530

    Feat/ google adsence

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,24 @@
 import "../styles/index.scss";
+import Script from "next/script";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { SSRProvider } from "react-bootstrap";
 import { ChakraProvider } from '@chakra-ui/react'
 function MyApp({ Component, pageProps }) {
-  return (
-    <SSRProvider>
+  return (<>
+  <Script id="Adsense-id" 
+  async strategy="afterInteractive"
+  onError={ (e) => { console.error('Script failed to load', e) }}
+src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2326546523711729"
+crossorigin="anonymous"
+/>
+  <SSRProvider>
       <ChakraProvider>
 
       <Component {...pageProps} />
       </ChakraProvider>
     </SSRProvider>
+  </>
+    
   );
 }
 
